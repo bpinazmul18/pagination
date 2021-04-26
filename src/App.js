fcf11a1e5ad8107ba4c790e86abf8f02b04e70fc
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-04-27 04:06:43 +0600

    next and prev button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,30 @@ function App() {
     setFollowers(data[page]);
   }, [loading, page]);
 
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
   return (
     <main>
       <div className="section-title">
@@ -27,17 +51,23 @@ function App() {
 
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
             {data.map((item, index) => {
               return (
                 <button
                   className={`page-btn ${index === page ? "active-btn" : null}`}
                   key={index}
-                  onClick={() => setPage(index)}
+                  onClick={() => handlePage(index)}
                 >
                   {index + 1}
                 </button>
               );
             })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </section>
