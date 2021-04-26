81960c564ccc2fdc3f8832ec0f3c6f85d2a565fa
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-04-27 02:28:57 +0600

    fetchData and display to list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  return (
+    <main>
+      <div className="section-title">
+        <h2>{loading ? "loading..." : "pagination"}</h2>
+        <div className="underline"></div>
+      </div>
+
+      <section className="followers">
+        <div className="container">
+          {data.map((follower, index) => {
+            return <Follower key={index} {...follower} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
