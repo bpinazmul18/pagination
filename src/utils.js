ea102ac1ec37d350d6ec211b56d89d858f774ac0
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-04-27 02:54:03 +0600

    add paginate logic

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const page = Math.ceil(followers.length / itemsPerPage);
+  const newFollowers = Array.from({ length: page }, (_, index) => {
+    const start = index * itemsPerPage;
+    console.log(start);
+    return followers.slice(start, start + itemsPerPage);
+  });
 
-export default paginate
+  return newFollowers;
+};
+
+export default paginate;
