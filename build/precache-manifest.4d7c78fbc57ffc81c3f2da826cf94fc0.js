bd5e56d6ad8ed00760e05a534d41bf66d9bdcb2e
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-04-27 04:12:53 +0600

    add build folder for production

---
+++ build/precache-manifest.4d7c78fbc57ffc81c3f2da826cf94fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68db0421126f500acd11ef899cfbeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bed6a4a107bb411ab33c",
+    "url": "/static/css/main.c088ef29.chunk.css"
+  },
+  {
+    "revision": "f890e1954844aa7faa34",
+    "url": "/static/js/2.3d099c39.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3d099c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed6a4a107bb411ab33c",
+    "url": "/static/js/main.62959be2.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
